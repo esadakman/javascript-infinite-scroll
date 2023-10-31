ae96d5ed304b78d808374d052d851632bff110d1
esad <esadd26@gmail.com>
2023-10-31 20:28:26 +0300

    function fixed

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 const scrollers = document.querySelectorAll(".scroller");
 
 // If a user hasn't opted in for recuded motion, then we add the animation
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
 
